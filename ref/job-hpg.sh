8e95119cf5da0dd32681dc43c0e2bcb200ff58b3
Jackson Wahl <jacksonwahl@login9.ufhpc>
2025-04-08 15:43:30 -0400

    adding run stuff

--- ref/job-hpg.sh
+++ ref/job-hpg.sh
@@ -2,11 +2,12 @@
 #SBATCH --account=eel6763
 #SBATCH --qos=eel6763
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks=8
 #SBATCH --ntasks-per-socket=1
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=500mb
 #SBATCH -t 00:05:00
 #SBATCH -o outfile
 #SBATCH -e errfile
-export OMP_NUM_THREADS=8
+export GMON_OUT_PREFIX=gmon.out-
 srun --mpi=$HPC_PMIX ./miniMD_openmpi
